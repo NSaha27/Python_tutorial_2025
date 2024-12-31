7e08c776275ad6ba7360856322d13bc847aa2efd
NSaha27 <sahaniladri27y@gmail.com>
2024-12-31 22:15:31 +0530

    Initial Python Commit

---
+++ Tutorials/01_Introduction/intro_01/continuation_char.py
@@ -0,0 +1,18 @@
+summation = 12 + 18 + 65 + \
+            20 + 17 + 8
+print(summation)
+
+print("Hello \
+World, \
+Welcome to \
+Python tutorial!")
+
+my_note = "Python is a high-level programming language. \
+It is a simple, general-purpose, \
+dynamic, platform-independant, \
+object-oriented programming language \
+used to develop almost any type of application. \
+It is developed by Guido Van Rossum in the year 1991 \
+and its first version was released in 1994."
+
+print(my_note)
---
+++ Tutorials/02_variables_and_keywords/var_01/main.py
@@ -0,0 +1,19 @@
+# simple variable declaration
+username = "niladrisaha"
+name = "Niladri Saha"
+phone = "8981684201"
+email = "sahaniladri25y@gmail.com"
+password = "Nil12345"
+
+print("User details:"), print("Username:", username), print("Name:", name), print("Phone:", phone), print("Email Id:", email), print("Password:", password)
+
+print()
+
+# variable declaration in single line
+street, city, state, zipcode = "Rani Rasmoni Avenue", "Kolkata", "West Bengal", "700075"
+
+print("Address details:")
+print("Street:", street)
+print("City:", city)
+print("State:", state)
+print("Zip Code:", zipcode)
